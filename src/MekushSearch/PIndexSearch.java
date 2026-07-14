888ec5bce6ad9a9b47cc41486a26e465e6b6469f
Kush <kushme1113@gmail.com>
2026-07-14 15:01:13 +0530

    Added new DSA programs

--- src/MekushSearch/PIndexSearch.java
+++ src/MekushSearch/PIndexSearch.java
@@ -1,4 +1,33 @@
 package MekushSearch;
 
-public class PIndexSearch {
+import java.util.Scanner;
+
+public class PIndexSearch {  // it is a mountain array
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the size of the array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.print("enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        int low = 0, high = n - 1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                System.out.println("peak index is: "+mid+" and peak is: "+arr[mid]);
+                break;
+            }
+            else if(arr[mid]<arr[mid+1]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+
+        }
+
+
+    }
 }
